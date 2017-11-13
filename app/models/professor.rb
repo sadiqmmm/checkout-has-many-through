2ebe9128e90defb8bdfa0d600191edd270628e5f
Mohammed Sadiq <sadiqmmm@gmail.com>
2017-11-14 02:00:23 +0530

    Add dependent destroy to remove the relationship in the through table between the professor and expertise_professor record id

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,4 +1,4 @@
 class Professor < ApplicationRecord
-	has_many :expertise_professors
+	has_many :expertise_professors, dependent: :destroy
 	has_many :expertises, through: :expertise_professors
 end
